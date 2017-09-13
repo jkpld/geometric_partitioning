d0bfcba3ec3231eeb7ad5c66ead5d31f3d75edb4
James Kapaldo <james.kapaldo@gmail.com>
2017-09-12 21:54:19 -0400

    Fix: Add error control

--- geometric_partition.m
+++ geometric_partition.m
@@ -1,4 +1,4 @@
-function [cuts, Info] = geometric_partition(B, N, K, I, S, r0, options)
+function [cuts, Info] = geometric_partition(B, N, K, I, S, r0, options, objNumber)
 % GEOMETRIC_PARTITION
 %
 % Input parameters -
@@ -22,128 +22,123 @@ Info = [];
 DEBUG = options.Debug;
 DEBUG_PLOT = options.Debug_plot;
 
-
-% Compute the object paritions
-if DEBUG
-    timer_start = tic;
-    [objCuts,~,cutCenters,cutIndices,CreateObjectPartitionInfo] = createObjectPartitions(B,N,r0,options);
-    Info.CreateObjectPartition = CreateObjectPartitionInfo;
-    Info.originalCuts = objCuts;
-    Info.centers = r0;
-    if DEBUG_PLOT
-        [fig,ax] = plotObjectCutDebugInfo(B,N,objCuts,Info);
-        ax.Title.String = 'Cuts before optimization';
-        drawnow;
-        setTheme(fig,'dark')
-    end
-else
-    [objCuts,~,cutCenters,cutIndices] = createObjectPartitions(B,N,r0,options);
+if nargin < 8
+    objNumber = 1;
 end
 
-if isempty(objCuts)
-    return;
-else
-    % remove cuts that have a large negative curvature
-%     cutKs = K(cutIndices)
-%     toRemove = (min(cutKs,[],2) < -1/options.Max_Radius)
-end
-
-% Create the triangle groups
-triangleGroups = constrainedObjectCenterTriangulation(B,r0,options);
-
-% Get cut association before optimization
-[~,vrtA] = computeCutAdjacency(cutIndices,B,5);
-associatedCuts = findAssociatedCuts(triangleGroups,objCuts,r0,vrtA,cutCenters);
-
-% Optimize the cuts
-[objCuts,objCutNs,objCutKs,cutIndices] = optimizeCuts(cutIndices,B,N,K,options);
-
-if DEBUG
-    Info.optimizedCuts = objCuts;
-    Info.cutsIntersectBoundary = checkCutIntegrity(objCuts,B,options.Use_GPU);
-    
-    if DEBUG_PLOT
-        [fig,ax] = plotObjectCutDebugInfo(B,N,objCuts,Info);
-        ax.Title.String = 'Cuts after optimization';
-        drawnow;
-        setTheme(fig,'dark')
-    end
-end
-
-% Get cut association after optimization
-[~,vrtA] = computeCutAdjacency(cutIndices,B,5);
-associatedCutsAfterOptimization = findAssociatedCuts(triangleGroups,objCuts,r0,vrtA,cutCenters);
-
-% Combine associations before and after to get the complete
-% list.
-for i = 1:numel(associatedCuts)
-    associatedCuts{i} = unique([associatedCuts{i};associatedCutsAfterOptimization{i}]);
-end
-
-% Create triangle cuts
-[objTriCuts,objTriCutNs,objTriCutKs,~,InfoTri] = createTriangleCuts(B, N, K, triangleGroups, r0, associatedCuts, options);
-
-if DEBUG
-    
-%     if ~isempty(objTriCuts)
-%         for i=1:numel(InfoTri)
-%             InfoTri(i).originalCuts = InfoTri(i).originalCuts + [topLeftB, topLeftB];
-%             InfoTri(i).vertexOptimizedCuts = InfoTri(i).vertexOptimizedCuts + [topLeftB, topLeftB];
-%             InfoTri(i).optimizedCuts = InfoTri(i).optimizedCuts + [topLeftB, topLeftB];
-%             InfoTri(i).triangleCenterSearchPoints = InfoTri(i).triangleCenterSearchPoints + topLeftB;
-%         end
-%     end
-    Info.CreateTriangleCuts = InfoTri;
-    if DEBUG_PLOT
-        if isempty(triangleGroups)
-            fprintf('No triangle information to plot!\n')
-        else
-            [fig,ax] = plotObjectTriDebugInfo(B,objTriCuts,objTriCutKs,Info);
-            ax.Title.String = 'Triangle cuts';
+try
+    % Compute the object paritions
+    if DEBUG
+        timer_start = tic;
+        [objCuts,~,cutCenters,cutIndices,CreateObjectPartitionInfo] = createObjectPartitions(B,N,r0,options);
+        Info.CreateObjectPartition = CreateObjectPartitionInfo;
+        Info.originalCuts = objCuts;
+        Info.centers = r0;
+        if DEBUG_PLOT
+            [fig,ax] = plotObjectCutDebugInfo(B,N,objCuts,Info);
+            ax.Title.String = 'Cuts before optimization';
             drawnow;
-            setTheme(fig,'dark');
+            setTheme(fig,'dark')
+        end
+    else
+        [objCuts,~,cutCenters,cutIndices] = createObjectPartitions(B,N,r0,options);
+    end
+
+    if isempty(objCuts)
+        return;
+    end
+
+    % Create the triangle groups
+    triangleGroups = constrainedObjectCenterTriangulation(B,r0,options);
+
+    % Get cut association before optimization
+    [~,vrtA] = computeCutAdjacency(cutIndices,B,5);
+    associatedCuts = findAssociatedCuts(triangleGroups,objCuts,r0,vrtA,cutCenters);
+
+    % Optimize the cuts
+    [objCuts,objCutNs,objCutKs,cutIndices] = optimizeCuts(cutIndices,B,N,K,options);
+
+    if DEBUG
+        Info.optimizedCuts = objCuts;
+        Info.cutsIntersectBoundary = checkCutIntegrity(objCuts,B,options.Use_GPU);
+
+        if DEBUG_PLOT
+            [fig,ax] = plotObjectCutDebugInfo(B,N,objCuts,Info);
+            ax.Title.String = 'Cuts after optimization';
+            drawnow;
+            setTheme(fig,'dark')
         end
     end
+
+    % Get cut association after optimization
+    [~,vrtA] = computeCutAdjacency(cutIndices,B,5);
+    associatedCutsAfterOptimization = findAssociatedCuts(triangleGroups,objCuts,r0,vrtA,cutCenters);
+
+    % Combine associations before and after to get the complete
+    % list.
+    for i = 1:numel(associatedCuts)
+        associatedCuts{i} = unique([associatedCuts{i};associatedCutsAfterOptimization{i}]);
+    end
+
+    % Create triangle cuts
+    [objTriCuts,objTriCutNs,objTriCutKs,~,InfoTri] = createTriangleCuts(B, N, K, triangleGroups, r0, associatedCuts, options);
+
+    if DEBUG
+
+    %     if ~isempty(objTriCuts)
+    %         for i=1:numel(InfoTri)
+    %             InfoTri(i).originalCuts = InfoTri(i).originalCuts + [topLeftB, topLeftB];
+    %             InfoTri(i).vertexOptimizedCuts = InfoTri(i).vertexOptimizedCuts + [topLeftB, topLeftB];
+    %             InfoTri(i).optimizedCuts = InfoTri(i).optimizedCuts + [topLeftB, topLeftB];
+    %             InfoTri(i).triangleCenterSearchPoints = InfoTri(i).triangleCenterSearchPoints + topLeftB;
+    %         end
+    %     end
+        Info.CreateTriangleCuts = InfoTri;
+        if DEBUG_PLOT
+            if isempty(triangleGroups)
+                fprintf('No triangle information to plot!\n')
+            else
+                [fig,ax] = plotObjectTriDebugInfo(B,objTriCuts,objTriCutKs,Info);
+                ax.Title.String = 'Triangle cuts';
+                drawnow;
+                setTheme(fig,'dark');
+            end
+        end
+    end
+
+    % Determine winning cuts
+    [~,vrtA] = computeCutAdjacency(cutIndices,B,1);
+    [objCuts,~,objCutKs,isTriCut,scores] = chooseWinningCuts(objCuts,objCutNs,objCutKs,objTriCuts,objTriCutNs,objTriCutKs,associatedCuts,vrtA,S,I);
+
+    if DEBUG
+        Info.cutScores = scores;
+    end
+
+    % Remove any cuts that are not at concave points.
+    if ~isempty(objCuts) && any(~isTriCut)
+    %     tmpK = objCutKs;
+    %     tmpK(isnan(tmpK)) = inf;
+    %     toRemove = ~all(tmpK < 1/options.Max_Radius,2);
+    %     toRemove = (mean(objCutKs,2) < 1/(2*options.Max_Radius)) & ~isTriCut; % maybe use max instead of mean.
+    % objCutKs
+    % 1/options.Max_Radius
+        toRemove = ((max(objCutKs,[],2) < 1/options.Max_Radius) | (min(objCutKs,[],2) < -1/(options.Max_Radius))) & ~isTriCut; % maybe use max instead of mean.
+    %     toRemove = any(objCutKs < 1/(2*options.Max_Radius),2) & ~isTriCut;
+        objCuts(toRemove,:) = [];
+    end
+    % objCuts
+    % Done!
+
+    cuts = objCuts;
+
+    if DEBUG
+        Info.cutCalculationTime = toc(timer_start);
+    end
+
+catch ME
+    cuts = [];
+    Info.error = ME;
+    fprintf('\nWarning! There was an error in object %d. Full error report stored in Info{%d}.error\n', objNumber, objNumber)
+    fprintf(2,'%s\n', getReport(Info.error,'basic'))
+    fprintf('\n')
 end
-
-% Determine winning cuts
-[~,vrtA] = computeCutAdjacency(cutIndices,B,1);
-[objCuts,~,objCutKs,isTriCut,scores] = chooseWinningCuts(objCuts,objCutNs,objCutKs,objTriCuts,objTriCutNs,objTriCutKs,associatedCuts,vrtA,S,I);
-
-if DEBUG
-    Info.cutScores = scores;
-end
-
-% Remove any cuts that are not at concave points.
-if ~isempty(objCuts) && any(~isTriCut)
-%     tmpK = objCutKs;
-%     tmpK(isnan(tmpK)) = inf;
-%     toRemove = ~all(tmpK < 1/options.Max_Radius,2);
-%     toRemove = (mean(objCutKs,2) < 1/(2*options.Max_Radius)) & ~isTriCut; % maybe use max instead of mean.
-% objCutKs
-% 1/options.Max_Radius
-    toRemove = ((max(objCutKs,[],2) < 1/options.Max_Radius) | (min(objCutKs,[],2) < -1/(options.Max_Radius))) & ~isTriCut; % maybe use max instead of mean.
-%     toRemove = any(objCutKs < 1/(2*options.Max_Radius),2) & ~isTriCut;
-    objCuts(toRemove,:) = [];
-end
-% objCuts
-% Done!
-
-% Offset the cuts by the topLeftB
-% objCuts(:,1:2) = objCuts(:,1:2) + topLeftB;
-% objCuts(:,3:4) = objCuts(:,3:4) + topLeftB;
-% 
-% % Get the linear indices of the pixels for each cut.
-% cutPixList_cell = cell(size(objCuts,1),1);
-% for current_cut = 1:size(objCuts,1)
-%     inds = rayTrace(objCuts(current_cut,1:2),objCuts(current_cut,3:4));
-%     cutPixList_cell{current_cut} = inds(:,1) + (inds(:,2)-1)*numImRows;
-% end
-% 
-% cutPixList = cat(1,cutPixList_cell{:});
-cuts = objCuts;
-
-if DEBUG
-    Info.cutCalculationTime = toc(timer_start);
-end
-
